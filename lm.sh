cdc8021b9faacc8941a652a6515e05a382eabaf3
Kwang-Ho Kim <kimkwangho82@gmail.com>
2015-02-12 09:17:17 +0000

    WSJ shell 생성

--- lm.sh
+++ lm.sh
@@ -1,1 +1,3 @@
 th lm.lua --train train.txt -- vocab vocab.txt
+
+th lm.lua --train ./data/wsj37000k.org.sentence.txt.upper.voc20k_unk.shuffle.head3700k --valid ./data/wsj37000k.org.sentence.txt.upper.voc20k_unk.shuffle.tail_16255sent --test ./data/wsj37000k.org.sentence.txt.upper.voc20k_unk.shuffle.tail_16255sent --vocab ./data/wsj_20k.vocab --n 3
