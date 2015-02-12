911e430eb55ce894474367d57e63a78b75435fb2
Kwang-Ho Kim <kimkwangho82@gmail.com>
2015-02-12 09:22:37 +0000

    torch_nn을 이용한 feed-forward lm 생성

---
+++ torch_nn/main.lua
@@ -0,0 +1,13 @@
+require 'train'
+require 'predict_next_word'
+
+use_manual_technique = false;
+epochs = 1;
+
+-- Manual Training seems to require more epochs to get a similar error rate.
+if use_manual_technique == true then epochs = 3; end
+
+model = train(epochs,use_manual_technique);
+
+predict_next_word('life', 'in', 'new', model, 3);
+predict_next_word('do', 'you', 'know', model, 250);
